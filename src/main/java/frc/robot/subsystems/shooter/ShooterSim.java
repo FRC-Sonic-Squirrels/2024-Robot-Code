a1da457599e17321172d73fce598758ca9935502
Tim <timellis337@gmail.com>
2024-01-13 10:55:43 -0800

    add sim class

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSim.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.shooter;
+
+public class ShooterSim implements ShooterIO{
+    public ShooterSim() {}
+
+    @Override
+    public void updateInputs(ShooterIOInputs inputs) {}
+}
