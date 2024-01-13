de811f7354ae7254ea1e88ec72ea691aa5480364
Tim <timellis337@gmail.com>
2024-01-13 10:49:10 -0800

    base shooter subsystem

---
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.shooter.ShooterIO.ShooterIOInputs;
+
+public class Shooter extends Command {
+  private final ShooterIO io;
+  private final ShooterIOInputs inputs = new ShooterIOInputs();
+
+  /** Creates a new ShooterSubsystem. */
+  public Shooter(ShooterIO io) {
+    this.io = io;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    io.updateInputs(inputs);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems.shooter;
+
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
+
+public interface ShooterIO {
+    /** Contains all of the input data received from hardware. */
+    public static class ShooterIOInputs implements LoggableInputs{
+        public void toLog(LogTable table) {}
+        public void fromLog(LogTable table) {}
+    }
+
+    /** Updates the set of loggable inputs. */
+    public default void updateInputs(ShooterIOInputs inputs) {}
+    
+}
