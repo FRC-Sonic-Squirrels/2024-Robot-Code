a1d4e23d8c945e8f4ff3ab82e873aac160ae608b
Tim <timellis337@gmail.com>
2024-01-13 11:02:46 -0800

    climber base

---
+++ src/main/java/frc/robot/subsystems/climber/ClimberReal.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.climber;
+
+public class ClimberReal implements ClimberIO{
+    public ClimberReal() {}
+
+    @Override
+    public void updateInputs(ClimberIOInputs inputs) {}
+}
