8b71fbeaf395463a7791a76391da9988116f246a
Tim <timellis337@gmail.com>
2024-01-13 11:09:48 -0800

    intake base

---
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIO.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems.Intake;
+
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
+
+public interface IntakeIO {
+    /** Contains all of the input data received from hardware. */
+    public static class IntakeIOInputs implements LoggableInputs{
+        public void toLog(LogTable table) {}
+        public void fromLog(LogTable table) {}
+    }
+
+    /** Updates the set of loggable inputs. */
+    public default void updateInputs(IntakeIOInputs inputs) {}
+    
+}
---
+++ src/main/java/frc/robot/subsystems/Intake/IntakeSim.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.Intake;
+
+public class IntakeSim implements IntakeIO{
+    public IntakeSim() {}
+
+    @Override
+    public void updateInputs(IntakeIOInputs inputs) {}
+}
