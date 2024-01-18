ffde622bea3b9b11dacf1f636c4b105e5dee124e
LuminousLlama <keyushattarde@gmail.com>
2024-01-18 15:32:36 -0800

    update arm code

--- src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
@@ -8,23 +8,33 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.Constants;
 
 public class ArmIOReal implements ArmIO {
-  private static final double RADS_TO_ROTATIONS = 0.0;
+  // place holder numbers
+  private static final double GEAR_RATIO =
+      (60.0 / 12.0) * (50.0 / 36.0) * (50.0 / 18.0) * (58.0 / 10.0);
+  // adapted from
+  // https://pro.docs.ctr-electronics.com/en/latest/docs/api-reference/device-specific/talonfx/closed-loop-requests.html#converting-from-meters
+  private static final double OUTPUT_RADS_TO_MOTOR_ROTATIONS = (1 / (2 * Math.PI)) * GEAR_RATIO;
+  private static final double OUTPUT_ROTATIONS_TO_OUTPUT_RADS = 2 * Math.PI;
 
   private final StatusSignal<Double> appliedVols;
   private final StatusSignal<Double> positionRotations;
   private final StatusSignal<Double> currentAmps;
   private final StatusSignal<Double> tempCelsius;
 
-  // FIXME:
+  private final MotionMagicVoltage closedLoopControl =
+      new MotionMagicVoltage(0.0).withEnableFOC(true);
+  private final VoltageOut openLoopControl = new VoltageOut(0.0).withEnableFOC(true);
+
   private final TalonFX motor;
 
   public ArmIOReal() {
-    // FIXME
-    motor = new TalonFX(0);
+    motor = new TalonFX(Constants.CanIDs.ARM_CAN_ID);
 
     TalonFXConfiguration config = new TalonFXConfiguration();
 
@@ -34,14 +44,16 @@ public class ArmIOReal implements ArmIO {
     config.CurrentLimits.SupplyCurrentLimitEnable = true;
 
     config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-    // closed loop configuration is handled by setClosedLoopConstants()
+
+    config.Feedback.SensorToMechanismRatio = GEAR_RATIO;
+    config.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+    // other closed loop configuration is handled by setClosedLoopConstants()
 
     appliedVols = motor.getMotorVoltage();
     positionRotations = motor.getPosition();
     currentAmps = motor.getStatorCurrent();
     tempCelsius = motor.getDeviceTemp();
 
-    // BaseStatusSignal.setUpdateFrequencyForAll(100, )
     BaseStatusSignal.setUpdateFrequencyForAll(100, appliedVols, positionRotations);
     BaseStatusSignal.setUpdateFrequencyForAll(50, currentAmps);
     BaseStatusSignal.setUpdateFrequencyForAll(1, tempCelsius);
@@ -53,7 +65,9 @@ public class ArmIOReal implements ArmIO {
   public void updateInputs(ArmIOInputs inputs) {
     BaseStatusSignal.refreshAll(appliedVols, positionRotations, currentAmps, tempCelsius);
     // could look into latency compensating this value
-    inputs.armPositionRad = (positionRotations.getValueAsDouble() / RADS_TO_ROTATIONS);
+    inputs.armPosition =
+        Rotation2d.fromRadians(
+            positionRotations.getValueAsDouble() * OUTPUT_ROTATIONS_TO_OUTPUT_RADS);
 
     inputs.armAppliedVolts = appliedVols.getValueAsDouble();
     inputs.armCurrentAmps = currentAmps.getValueAsDouble();
@@ -62,21 +76,19 @@ public class ArmIOReal implements ArmIO {
 
   @Override
   public void setClosedLoopPosition(Rotation2d angle) {
-    var rotations = angle.getRadians() * RADS_TO_ROTATIONS;
-    var control = new MotionMagicVoltage(rotations).withEnableFOC(true);
-    // .withFeedForward(/*compensate for gravity here? */)
+    var rotations = angle.getRadians() * OUTPUT_RADS_TO_MOTOR_ROTATIONS;
+    closedLoopControl.withPosition(rotations);
 
-    motor.setControl(control);
+    motor.setControl(closedLoopControl);
   }
 
   @Override
   public void setClosedLoopConstants(
-      double kP, double kD, double maxProfiledVelocity, double maxProfiledAcceleration) {
+      double kP, double kD, double kG, double maxProfiledVelocity, double maxProfiledAcceleration) {
     Slot0Configs pidConfig = new Slot0Configs();
-    // this could be something to explore
-    // pidConfig.GravityType
     pidConfig.kP = kP;
     pidConfig.kD = kD;
+    pidConfig.kG = kG;
 
     MotionMagicConfigs mmConfig = new MotionMagicConfigs();
     mmConfig.MotionMagicCruiseVelocity = maxProfiledAcceleration;
@@ -88,6 +100,6 @@ public class ArmIOReal implements ArmIO {
 
   @Override
   public void setVoltage(double volts) {
-    motor.setControl(new VoltageOut(volts).withEnableFOC(true));
+    motor.setControl(openLoopControl.withOutput(volts));
   }
 }
