b41efed669a7a355f8a018c90ab09ef5de0ffaa7
LuminousLlama <keyushattarde@gmail.com>
2024-01-17 17:00:48 -0800

    initial pre testing arm code

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -4,21 +4,78 @@
 
 package frc.robot.subsystems.arm;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.team6328.LoggedTunableNumber;
+import frc.robot.Constants;
+import frc.robot.Constants.RobotMode.RobotType;
 import org.littletonrobotics.junction.Logger;
 
 public class Arm extends SubsystemBase {
   private final ArmIO io;
   private final ArmIOInputsAutoLogged inputs = new ArmIOInputsAutoLogged();
 
+  private static final String ROOT_TABLE = "Arm";
+  private static final LoggedTunableNumber kP = new LoggedTunableNumber(ROOT_TABLE + "/kP");
+  private static final LoggedTunableNumber kD = new LoggedTunableNumber(ROOT_TABLE + "/kD");
+
+  private static final LoggedTunableNumber closedLoopMaxVelocityConstraint =
+      new LoggedTunableNumber(ROOT_TABLE + "/defaultClosedLoopMaxVelocityConstraint");
+  private static final LoggedTunableNumber closedLoopMaxAccelerationConstraint =
+      new LoggedTunableNumber(ROOT_TABLE + "/defaultClosedLoopMaxVelocityConstraint");
+
+  static {
+    if (Constants.RobotMode.getRobot() == RobotType.ROBOT_2024) {
+      kP.initDefault(0.0);
+      kD.initDefault(0);
+
+      closedLoopMaxVelocityConstraint.initDefault(0);
+      closedLoopMaxAccelerationConstraint.initDefault(0);
+    } else if (Constants.RobotMode.getRobot() == RobotType.ROBOT_SIMBOT) {
+
+      kP.initDefault(1.0);
+      kD.initDefault(0);
+
+      closedLoopMaxVelocityConstraint.initDefault(40);
+      closedLoopMaxAccelerationConstraint.initDefault(80);
+    }
+  }
+
   /** Creates a new ArmSubsystem. */
   public Arm(ArmIO io) {
     this.io = io;
+
+    io.setClosedLoopConstants(
+        kP.get(),
+        kD.get(),
+        closedLoopMaxVelocityConstraint.get(),
+        closedLoopMaxAccelerationConstraint.get());
   }
 
   @Override
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("Arm", inputs);
+
+    // ---- UPDATE TUNABLE NUMBERS
+    var hc = hashCode();
+    if (kP.hasChanged(hc)
+        || kD.hasChanged(hc)
+        || closedLoopMaxVelocityConstraint.hasChanged(hc)
+        || closedLoopMaxAccelerationConstraint.hasChanged(hc)) {
+      io.setClosedLoopConstants(
+          kP.get(),
+          kP.get(),
+          closedLoopMaxVelocityConstraint.get(),
+          closedLoopMaxAccelerationConstraint.get());
+    }
+  }
+
+  public void setAngle(Rotation2d angle) {
+    io.setClosedLoopPosition(angle);
+  }
+
+  public Rotation2d getAngle() {
+    return Rotation2d.fromRadians(inputs.armPositionRad);
   }
 }
