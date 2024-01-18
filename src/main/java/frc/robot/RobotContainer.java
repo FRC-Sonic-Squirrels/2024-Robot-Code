ed95c645de10899563c4067ddbf2c5cfee63f519
LuminousLlama <keyushattarde@gmail.com>
2024-01-18 15:22:39 -0800

    fix mechanism visualization

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -325,5 +325,6 @@ public class RobotContainer {
 
   public void updateVisualization() {
     SimpleMechanismVisualization.updateVisualization(arm.getAngle());
+    SimpleMechanismVisualization.logMechanism();
   }
 }
