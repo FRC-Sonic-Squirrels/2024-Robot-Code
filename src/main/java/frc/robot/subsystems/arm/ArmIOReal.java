f01fb23e452740d9efbb08edfbda42e883f0b16a
LuminousLlama <keyushattarde@gmail.com>
2024-01-17 17:00:48 -0800

    initial pre testing arm code

--- src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
@@ -1,8 +1,93 @@
 package frc.robot.subsystems.arm;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public class ArmIOReal implements ArmIO {
-  public ArmIOReal() {}
+  private static final double RADS_TO_ROTATIONS = 0.0;
+
+  private final StatusSignal<Double> appliedVols;
+  private final StatusSignal<Double> positionRotations;
+  private final StatusSignal<Double> currentAmps;
+  private final StatusSignal<Double> tempCelsius;
+
+  // FIXME:
+  private final TalonFX motor;
+
+  public ArmIOReal() {
+    // FIXME
+    motor = new TalonFX(0);
+
+    TalonFXConfiguration config = new TalonFXConfiguration();
+
+    config.CurrentLimits.SupplyCurrentThreshold = 0.0;
+    config.CurrentLimits.SupplyTimeThreshold = 0.0;
+    config.CurrentLimits.SupplyCurrentLimit = 0.0;
+    config.CurrentLimits.SupplyCurrentLimitEnable = true;
+
+    config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    // closed loop configuration is handled by setClosedLoopConstants()
+
+    appliedVols = motor.getMotorVoltage();
+    positionRotations = motor.getPosition();
+    currentAmps = motor.getStatorCurrent();
+    tempCelsius = motor.getDeviceTemp();
+
+    // BaseStatusSignal.setUpdateFrequencyForAll(100, )
+    BaseStatusSignal.setUpdateFrequencyForAll(100, appliedVols, positionRotations);
+    BaseStatusSignal.setUpdateFrequencyForAll(50, currentAmps);
+    BaseStatusSignal.setUpdateFrequencyForAll(1, tempCelsius);
+
+    motor.optimizeBusUtilization();
+  }
 
   @Override
-  public void updateInputs(ArmIOInputs inputs) {}
+  public void updateInputs(ArmIOInputs inputs) {
+    BaseStatusSignal.refreshAll(appliedVols, positionRotations, currentAmps, tempCelsius);
+    // could look into latency compensating this value
+    inputs.armPositionRad = (positionRotations.getValueAsDouble() / RADS_TO_ROTATIONS);
+
+    inputs.armAppliedVolts = appliedVols.getValueAsDouble();
+    inputs.armCurrentAmps = currentAmps.getValueAsDouble();
+    inputs.armTempCelsius = tempCelsius.getValueAsDouble();
+  }
+
+  @Override
+  public void setClosedLoopPosition(Rotation2d angle) {
+    var rotations = angle.getRadians() * RADS_TO_ROTATIONS;
+    var control = new MotionMagicVoltage(rotations).withEnableFOC(true);
+    // .withFeedForward(/*compensate for gravity here? */)
+
+    motor.setControl(control);
+  }
+
+  @Override
+  public void setClosedLoopConstants(
+      double kP, double kD, double maxProfiledVelocity, double maxProfiledAcceleration) {
+    Slot0Configs pidConfig = new Slot0Configs();
+    // this could be something to explore
+    // pidConfig.GravityType
+    pidConfig.kP = kP;
+    pidConfig.kD = kD;
+
+    MotionMagicConfigs mmConfig = new MotionMagicConfigs();
+    mmConfig.MotionMagicCruiseVelocity = maxProfiledAcceleration;
+    mmConfig.MotionMagicAcceleration = maxProfiledAcceleration;
+
+    motor.getConfigurator().apply(pidConfig);
+    motor.getConfigurator().apply(mmConfig);
+  }
+
+  @Override
+  public void setVoltage(double volts) {
+    motor.setControl(new VoltageOut(volts).withEnableFOC(true));
+  }
 }
--- src/main/java/frc/robot/subsystems/arm/ArmIOSim.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOSim.java
@@ -1,8 +1,78 @@
 package frc.robot.subsystems.arm;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+
 public class ArmIOSim implements ArmIO {
-  public ArmIOSim() {}
+  private double armGearing = 6.0;
+  private double momentOfInertia = 1.4;
+  private double armLengthMeters = Units.inchesToMeters(12);
+
+  private double minAngleRad = Units.degreesToRadians(270);
+  private double maxAngleRad = Units.degreesToRadians(90);
+
+  SingleJointedArmSim armSim =
+      new SingleJointedArmSim(
+          DCMotor.getFalcon500Foc(1),
+          armGearing,
+          momentOfInertia,
+          armLengthMeters,
+          minAngleRad,
+          maxAngleRad,
+          false,
+          0);
+
+  private final ProfiledPIDController feedback;
+
+  private boolean closedLoop = false;
+  private Rotation2d closedLoopTargetAngle = new Rotation2d();
+  private double openLoopVolts = 0.0;
+
+  public ArmIOSim() {
+    feedback = new ProfiledPIDController(0, 0, 0, new Constraints(0, 0));
+  }
 
   @Override
-  public void updateInputs(ArmIOInputs inputs) {}
+  public void updateInputs(ArmIOInputs inputs) {
+
+    if (closedLoop) {
+      var controlEffort =
+          feedback.calculate(inputs.armPositionRad, closedLoopTargetAngle.getRadians());
+
+      armSim.setInputVoltage(controlEffort);
+    } else {
+      armSim.setInputVoltage(openLoopVolts);
+    }
+
+    armSim.update(0.02);
+
+    inputs.armPositionRad = armSim.getAngleRads();
+    inputs.armAppliedVolts = armSim.getOutput(0);
+    inputs.armCurrentAmps = armSim.getCurrentDrawAmps();
+  }
+
+  @Override
+  public void setVoltage(double volts) {
+    openLoopVolts = volts;
+    closedLoop = false;
+  }
+
+  @Override
+  public void setClosedLoopPosition(Rotation2d angle) {
+    closedLoop = true;
+    closedLoopTargetAngle = angle;
+  }
+
+  @Override
+  public void setClosedLoopConstants(
+      double kP, double kD, double maxProfiledVelocity, double maxProfiledAcceleration) {
+
+    feedback.setP(kP);
+    feedback.setD(kD);
+    feedback.setConstraints(new Constraints(maxProfiledVelocity, maxProfiledAcceleration));
+  }
 }
