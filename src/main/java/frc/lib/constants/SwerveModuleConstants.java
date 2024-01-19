0b4f4576bb743a1c26e766c91986c0924784989c
Tim <timellis337@gmail.com>
2024-01-18 19:36:15 -0800

    add L3 (plus speed kit) gearing

--- src/main/java/frc/lib/constants/SwerveModuleConstants.java
+++ src/main/java/frc/lib/constants/SwerveModuleConstants.java
@@ -10,6 +10,10 @@ public class SwerveModuleConstants {
 
     public static final double LEVEL_2_GEARING_DRIVE_GEAR_RATIO =
         (50.0 / 14.0) * (17.0 / 27.0) * (45.0 / 15.0);
-    public static final double LEVEL_2_GEARING_TURN_GEAR_RATIO = 150.0 / 7.0;
+
+    public static final double LEVEL_3_GEARING_DRIVE_GEAR_RATIO_PLUS_SPEED_KIT =
+        (50.0 / 16.0) * (16.0 / 28.0) * (45.0 / 15.0);
+
+    public static final double GEARING_TURN_GEAR_RATIO = 150.0 / 7.0;
   }
 }
