d1608fdb5c9b95f0387e717b3047890e4a6583bb
Tim <timellis337@gmail.com>
2024-01-20 23:28:33 -0800

    update geom util

--- src/main/java/frc/lib/team2930/GeometryUtil.java
+++ src/main/java/frc/lib/team2930/GeometryUtil.java
@@ -7,4 +7,8 @@ public class GeometryUtil {
   public static Rotation2d getHeading(Translation2d pose, Translation2d targetPose) {
     return new Rotation2d(targetPose.getX() - pose.getX(), targetPose.getY() - pose.getY());
   }
+
+  public static double getDist(Translation2d pose, Translation2d pose2) {
+    return Math.hypot(pose.getX() - pose2.getX(), pose.getY() - pose2.getY());
+  }
 }
