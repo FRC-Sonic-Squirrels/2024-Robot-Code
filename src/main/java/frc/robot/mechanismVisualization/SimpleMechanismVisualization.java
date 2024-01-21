bff30bbc68ef436c2d0a6a5977707df850b23d13
Tim <timellis337@gmail.com>
2024-01-20 20:19:32 -0800

    better visualizer

--- src/main/java/frc/robot/mechanismVisualization/SimpleMechanismVisualization.java
+++ src/main/java/frc/robot/mechanismVisualization/SimpleMechanismVisualization.java
@@ -19,11 +19,11 @@ public class SimpleMechanismVisualization {
   static MechanismLigament2d arm =
       armRoot.append(new MechanismLigament2d("arm", Units.inchesToMeters(12), 270));
 
-  static Mechanism2d shooterMechanism2d = new Mechanism2d(Units.inchesToMeters(20.0), 8);
+  static Mechanism2d shooterMechanism2d =
+      new Mechanism2d(Units.inchesToMeters(32.0), Units.inchesToMeters(50.0));
 
   static MechanismRoot2d shooterRoot =
-      shooterMechanism2d.getRoot(
-          "shooterRoot", 0.0, Constants.ShooterConstants.SHOOTER_OFFSET_METERS);
+      shooterMechanism2d.getRoot("shooterRoot", Units.inchesToMeters(27), Units.inchesToMeters(4));
 
   static MechanismLigament2d shooter =
       shooterRoot.append(
