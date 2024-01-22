3d60e6f25f7873d74ab4651d50608d66728680bb
Tim <timellis337@gmail.com>
2024-01-21 20:55:14 -0800

    intake use wpilib dcmotorsim

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -1,20 +1,30 @@
 package frc.robot.subsystems.intake;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.sim.TalonFXSimState;
-import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import frc.robot.Constants;
 
 public class IntakeIOSim implements IntakeIO {
 
-  private TalonFX motor = new TalonFX(0);
-  private TalonFXSimState motorSim = motor.getSimState();
+  private DCMotorSim motor =
+      new DCMotorSim(
+          DCMotor.getFalcon500(1),
+          Constants.IntakeConstants.GEARING,
+          Constants.IntakeConstants.MOI);
+
+  private double voltage = 0.0;
 
   public IntakeIOSim() {}
 
   @Override
   public void updateInputs(IntakeIOInputs inputs) {
-    motorSim = motor.getSimState();
-    motorSim.setSupplyVoltage(RobotController.getBatteryVoltage());
-    inputs.currentAmps = motorSim.getSupplyCurrent();
+    motor.setInputVoltage(voltage);
+    inputs.currentAmps = motor.getCurrentDrawAmps();
+    inputs.RPM = motor.getAngularVelocityRPM();
+  }
+
+  @Override
+  public void setVoltage(double volts) {
+    volts = voltage;
   }
 }
