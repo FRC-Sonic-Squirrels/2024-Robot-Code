7a8b9f661796140fc43f6a1fe923830030a7173e
Tim <timellis337@gmail.com>
2024-01-21 21:03:54 -0800

    add beam break

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.intake;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import frc.robot.Constants;
+import org.littletonrobotics.junction.networktables.LoggedDashboardBoolean;
 
 public class IntakeIOSim implements IntakeIO {
 
@@ -14,6 +15,8 @@ public class IntakeIOSim implements IntakeIO {
 
   private double voltage = 0.0;
 
+  private LoggedDashboardBoolean beamBreak = new LoggedDashboardBoolean("Intake/BeamBreak", false);
+
   public IntakeIOSim() {}
 
   @Override
@@ -21,6 +24,7 @@ public class IntakeIOSim implements IntakeIO {
     motor.setInputVoltage(voltage);
     inputs.currentAmps = motor.getCurrentDrawAmps();
     inputs.RPM = motor.getAngularVelocityRPM();
+    inputs.beamBreak = beamBreak.get();
   }
 
   @Override
