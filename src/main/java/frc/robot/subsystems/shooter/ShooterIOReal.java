8b8be0c4b5d8550d6577b89baf74906062e2d917
jayrencalub <thejayren.calub@gmail.com>
2024-01-22 19:31:03 -0800

    added shooter pivot constant

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOReal.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOReal.java
@@ -6,32 +6,30 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.Constants;
-import frc.robot.subsystems.arm.Arm;
 
 public class ShooterIOReal implements ShooterIO {
   TalonFX lead = new TalonFX(Constants.CanIDs.SHOOTER_LEAD_CAN_ID);
   TalonFX follow = new TalonFX(Constants.CanIDs.SHOOTER_FOLLOW_CAN_ID);
+  TalonFX pivot = new TalonFX(Constants.CanIDs.SHOOTER_PIVOT_CAN_ID);
 
-  private double RPM = 0.0;
   public Rotation2d pitch = new Rotation2d();
-
+  private double RPM = 0.0;
   public double launcherLeadTempCelsius = 0.0;
   public double launcherFollowTempCelsius = 0.0;
   public double launcherVoltage = 0.0;
-  
+
   public double pivotVoltage = 0.0;
   public double pivotTempCelsius = 0.0;
 
   private PIDController pivotController = new PIDController(0.01, 0, 0);
-  private Arm arm;
 
   Slot0Configs slot0Configs = new Slot0Configs();
   final VelocityVoltage request = new VelocityVoltage(0).withSlot(0);
-  
+
   public ShooterIOReal() {
     slot0Configs.kP = 0;
-    slot0Configs.kG = 0;
     slot0Configs.kD = 0;
+    slot0Configs.kG = 0;
     lead.getConfigurator().apply(slot0Configs);
     follow.getConfigurator().apply(slot0Configs);
   }
@@ -41,6 +39,8 @@ public class ShooterIOReal implements ShooterIO {
     // FIXME: get actual arm code
     inputs.pitch = new Rotation2d(/*arm.getAngle()*/ );
   }
+
+  // PIVOT
 
   @Override
   public void setPivotVel(double radPerSec) {}
@@ -73,7 +73,7 @@ public class ShooterIOReal implements ShooterIO {
     lead.setControl(request.withVelocity(rpm));
     follow.setControl(request.withVelocity(rpm));
   }
-  
+
   @Override
   public void setLauncherClosedLoopConstants(double kP, double kI, double kD) {}
 }
