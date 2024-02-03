47eb04551897c0c7ee7c1eeaeb599fd266517b23
jayrencalub <thejayren.calub@gmail.com>
2024-02-03 14:02:45 -0800

    removed rpm

--- src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
@@ -40,12 +40,11 @@ public class IntakeIOReal implements IntakeIO {
 
   @Override
   public void updateInputs(IntakeIOInputs inputs) {
-    BaseStatusSignal.refreshAll(currentAmps, velocityRPS, deviceTemp);
+    BaseStatusSignal.refreshAll(currentAmps, deviceTemp);
 
     motor.setControl(new VoltageOut(voltage));
 
     inputs.currentAmps = currentAmps.getValueAsDouble();
-    inputs.RPM = velocityRPS.getValueAsDouble() / 60.0;
     inputs.tempCelsius = deviceTemp.getValueAsDouble();
   }
 
