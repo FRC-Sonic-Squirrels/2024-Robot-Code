ae56098ec38a426979a8f6c2635d0ec4325f921d
jayrencalub <thejayren.calub@gmail.com>
2024-02-02 18:42:17 -0800

    elevator commands

--- src/main/java/frc/robot/commands/elevator/ElevatorMaxHeight.java
+++ src/main/java/frc/robot/commands/elevator/ElevatorMaxHeight.java
@@ -8,10 +8,10 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.Constants;
 
-public class ExtendElevator extends Command {
+public class ElevatorMaxHeight extends Command {
   Elevator elevator;
-  /** Creates a new ExtendElevator. */
-  public ExtendElevator(Elevator elevator) {
+  /** Creates a new ElevatorMaxHeight. */
+  public ElevatorMaxHeight(Elevator elevator) {
     this.elevator = elevator;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(elevator);
