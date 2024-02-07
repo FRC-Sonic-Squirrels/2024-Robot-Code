93a31d43ce142956dc990189b8a0a4b220159d6f
Tim <timellis337@gmail.com>
2024-02-06 17:21:08 -0800

    progress on shooting

--- src/main/java/frc/robot/visualization/GamepieceVisualization.java
+++ src/main/java/frc/robot/visualization/GamepieceVisualization.java
@@ -1,20 +1,24 @@
 package frc.robot.visualization;
 
+import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.Timer;
 import frc.lib.team2930.GeometryUtil;
 import frc.robot.Constants;
 import java.util.ArrayList;
 import org.littletonrobotics.junction.Logger;
 
 public class GamepieceVisualization {
-  private static ArrayList<Pose3d> poses = new ArrayList<>();
+  private static ArrayList<Pair<Pose3d, Double>> poses = new ArrayList<>();
+  private static Pose3d[] loggedPoses = new Pose3d[] {};
   private static double gamepieceSpacing = 0.4572;
   private static boolean prevGamepieceShot = false;
+  private static double timeout = 10.0;
 
   public static void updateVisualization(
       Pose2d robotPose,
@@ -30,8 +34,7 @@ public class GamepieceVisualization {
             robotToSpeaker.getX() * robotToSpeaker.getX()
                 + robotToSpeaker.getY() * robotToSpeaker.getY()
                 + robotToSpeaker.getZ() * robotToSpeaker.getZ());
-    double shooterTangentialVel =
-        shooterRPM / 60.0 * Constants.ShooterConstants.Launcher.WHEEL_DIAMETER_METERS * Math.PI;
+    double shooterTangentialVel = Constants.ShooterConstants.SHOOTING_SPEED;
 
     Logger.recordOutput("Visualization/ShooterTanSpeed", shooterTangentialVel);
     double horizShooterTangentialVel = shooterTangentialVel * Math.cos(shooterAngle.getRadians());
@@ -49,18 +52,29 @@ public class GamepieceVisualization {
     if (gamepieceShot && !prevGamepieceShot)
       for (int i = 0; i < (int) (dist / gamepieceSpacing); i++) {
         poses.add(
-            new Pose3d(
-                new Translation3d(
-                        gamepieceSpacing * i,
-                        new Rotation3d(0.0, -pitch.getRadians(), yaw.getRadians() + Math.PI))
-                    .plus(GeometryUtil.translation2dTo3d(robotPose.getTranslation())),
-                new Rotation3d(
-                    0.0, shooterAngle.getRadians(), robotPose.getRotation().getRadians())));
+            new Pair<Pose3d, Double>(
+                new Pose3d(
+                    new Translation3d(
+                            gamepieceSpacing * i,
+                            new Rotation3d(0.0, -pitch.getRadians(), yaw.getRadians() + Math.PI))
+                        .plus(GeometryUtil.translation2dTo3d(robotPose.getTranslation())),
+                    new Rotation3d(
+                        0.0, shooterAngle.getRadians(), robotPose.getRotation().getRadians())),
+                Timer.getFPGATimestamp()));
       }
+
+    for (int i = 0; i < poses.size(); i++) {
+      if (Timer.getFPGATimestamp() - poses.get(i).getSecond() >= timeout) poses.remove(i);
+    }
+
+    loggedPoses = new Pose3d[poses.size()];
+    for (int i = 0; i < poses.size(); i++) {
+      loggedPoses[i] = poses.get(i).getFirst();
+    }
     prevGamepieceShot = gamepieceShot;
   }
 
   public static void logTraj() {
-    Logger.recordOutput("Visualization/GamepieceTraj", poses.toArray(new Pose3d[] {}));
+    Logger.recordOutput("Visualization/GamepieceTraj", loggedPoses);
   }
 }
