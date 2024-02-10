b5ff763b8154cb6dc91a1d337f5d6c166ba617f7
LuminousLlama <keyushattarde@gmail.com>
2024-02-10 00:32:32 -0800

    spotless

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -292,5 +292,4 @@ public final class Constants {
         new Pose3d(0.0, 0.0, Units.inchesToMeters(38), new Rotation3d(0.0, 0.0, 0.0));
     public static final String CAMERA_NAME = "CAM2024_3";
   }
-
 }
