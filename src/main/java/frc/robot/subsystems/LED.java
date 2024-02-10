a5b56de41fbcaca5d16819e27e9e47c256cdcba9
jayrencalub <thejayren.calub@gmail.com>
2024-02-09 19:46:48 -0800

    added color objects

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -4,7 +4,6 @@
 
 package frc.robot.subsystems;
 
-import java.awt.Color;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.Timer;
@@ -19,8 +18,9 @@ public class LED extends SubsystemBase {
   individualLED led1 = new individualLED(0, 25);
   individualLED led2 = new individualLED(26, 60);
 
-  Color color1;
-  Color color2;
+  Color color1 = Color.RED;
+  Color color2 = Color.GREEN;
+  Color color3 = Color.BLUE;
 
   AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(led1.getLength() + led2.getLength());
   int snakeShade = 0;
