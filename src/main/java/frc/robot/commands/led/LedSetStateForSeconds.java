13c41f444a8f7a155c22fbfc373989df1e5c8b0b
jayrencalub <thejayren.calub@gmail.com>
2024-02-12 19:08:28 -0800

    changed argument for end call

--- src/main/java/frc/robot/commands/led/LedSetStateForSeconds.java
+++ src/main/java/frc/robot/commands/led/LedSetStateForSeconds.java
@@ -38,8 +38,7 @@ public class LedSetStateForSeconds extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    // FIXME: change argument to NOTHING after merge
-    led.setRobotState(null);
+    led.setRobotState(robotStates.NOTHING);
   }
 
   // Returns true when the command should end.
