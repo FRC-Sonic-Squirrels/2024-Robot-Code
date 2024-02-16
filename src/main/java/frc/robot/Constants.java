54feffbbc2db26f0ed8ea92f8fe2680a010cb5d3
Davide Massarenti <davide.m@corestack.io>
2024-02-15 21:05:19 -0800

    Spotless

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,7 +48,6 @@ public final class Constants {
   public class RobotMode {
     private static final RobotType ROBOT = RobotType.ROBOT_SIMBOT_REAL_CAMERAS;
 
-
     private static final Alert invalidRobotAlert =
         new Alert("Invalid robot selected, using competition robot as default.", AlertType.ERROR);
 
