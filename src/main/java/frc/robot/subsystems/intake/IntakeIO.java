51747473556294d8c2249709248eac6f73b2eca5
Tim <timellis337@gmail.com>
2024-02-15 17:48:22 -0800

    add back in beam break

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -10,6 +10,7 @@ public interface IntakeIO {
     public double currentAmps = 0.0;
     public double tempCelsius = 0.0;
     public double appliedVolts = 0.0;
+    public boolean beamBreak = false;
   }
 
   /** Updates the set of loggable inputs. */
