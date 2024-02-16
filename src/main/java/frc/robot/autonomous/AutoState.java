3e381667aeb4a7fa1c81e4474b07c871872c6e1c
Tim <timellis337@gmail.com>
2024-02-15 19:27:26 -0800

    more suggested changes

--- src/main/java/frc/robot/autonomous/AutoState.java
+++ src/main/java/frc/robot/autonomous/AutoState.java
@@ -1,8 +1,8 @@
 package frc.robot.autonomous;
 
 public class AutoState {
-  public AutoStates state = AutoStates.FOLLOW_PATH;
-  public double timestamp = 0.0;
+  public AutoStates state;
+  public double timestamp;
 
   public AutoState(AutoStates state, double timestamp) {
     this.state = state;
