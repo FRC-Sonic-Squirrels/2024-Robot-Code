741efec44cb3c873acac67f6d5a569e6209f5d17
LuminousLlama <keyushattarde@gmail.com>
2024-02-16 13:59:25 -0800

    temp default value for pid

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -36,12 +36,12 @@ public class Elevator extends SubsystemBase {
       closedLoopMaxAccelerationConstraint.initDefault(0.0);
 
     } else if (Constants.RobotMode.getRobot() == RobotType.ROBOT_2024) {
-      kP.initDefault(0.0);
+      kP.initDefault(1.0);
       kD.initDefault(0.0);
       kG.initDefault(0.0);
 
-      closedLoopMaxVelocityConstraint.initDefault(0.0);
-      closedLoopMaxAccelerationConstraint.initDefault(0.0);
+      closedLoopMaxVelocityConstraint.initDefault(10.0);
+      closedLoopMaxAccelerationConstraint.initDefault(10.0);
     }
   }
 
