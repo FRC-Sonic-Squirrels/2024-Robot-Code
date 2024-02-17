a5b744e393ca0cb32fd5b2f0dc1c88eda7e7507a
Tim <timellis337@gmail.com>
2024-02-17 12:09:43 -0800

    wrote middle auto code

---
+++ src/main/java/frc/robot/autonomous/substates/MiddleFirstSubstate.java
@@ -0,0 +1,110 @@
+package frc.robot.autonomous.substates;
+
+import com.choreo.lib.Choreo;
+import com.choreo.lib.ChoreoTrajectory;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.lib.team2930.StateMachine;
+import frc.robot.Constants;
+import frc.robot.DrivetrainWrapper;
+import frc.robot.autonomous.ChoreoHelper;
+import frc.robot.commands.ScoreSpeaker;
+import frc.robot.configs.RobotConfig;
+import frc.robot.subsystems.endEffector.EndEffector;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.visionGamepiece.ProcessedGamepieceData;
+import java.util.function.Supplier;
+
+public class MiddleFirstSubstate extends StateMachine {
+
+  private DrivetrainWrapper drive;
+  private Shooter shooter;
+  private EndEffector endEffector;
+  private Intake intake;
+  private RobotConfig config;
+  private ChoreoTrajectory traj;
+  private Supplier<ProcessedGamepieceData> closestGamepiece;
+  private ChoreoHelper choreoHelper;
+  private ScoreSpeaker scoreSpeaker;
+  private Trigger endEffectorTrigger;
+  private boolean prevEndEffectorBeamBreak = true;
+  private int gamepieceCounter = 0;
+  private boolean reachedCenter = false;
+
+  public MiddleFirstSubstate(
+      DrivetrainWrapper drive,
+      Shooter shooter,
+      EndEffector endEffector,
+      Intake intake,
+      RobotConfig config,
+      Supplier<ProcessedGamepieceData> closestGamepiece) {
+    this.drive = drive;
+    this.shooter = shooter;
+    this.endEffector = endEffector;
+    this.intake = intake;
+    this.config = config;
+    this.traj = Choreo.getTrajectory("middleAuto.1");
+    this.closestGamepiece = closestGamepiece;
+
+    setInitialState(this::initFollowPath);
+  }
+
+  private StateHandler initFollowPath() {
+    choreoHelper =
+        new ChoreoHelper(
+            timeFromStart(),
+            drive.getPoseEstimatorPose(),
+            this.traj,
+            config.getAutoTranslationPidController(),
+            config.getAutoTranslationPidController(),
+            config.getAutoThetaPidController());
+
+    endEffectorTrigger = new Trigger(endEffector::intakeSideTOFDetectGamepiece).debounce(0.1);
+
+    return this::followPath;
+  }
+
+  private StateHandler followPath() {
+    var chassisSpeeds =
+        choreoHelper.calculateChassisSpeeds(drive.getPoseEstimatorPose(), timeFromStart());
+
+    if (endEffectorTrigger.getAsBoolean() && !prevEndEffectorBeamBreak) {
+      gamepieceCounter++;
+      scoreSpeaker =
+          new ScoreSpeaker(drive, shooter, () -> true, gamepieceCounter == 3 ? 0.71 : 0.88);
+      scoreSpeaker.schedule();
+    }
+
+    if (gamepieceCounter <= 2
+        || (gamepieceCounter == 3
+            && closestGamepiece.get().distance <= Constants.AutoConstants.DIST_TO_START_INTAKING)) {
+      intake.setPercentOut(0.8);
+    } else {
+      intake.setPercentOut(0.0);
+    }
+
+    prevEndEffectorBeamBreak = endEffectorTrigger.getAsBoolean();
+
+    if (drive.getPoseEstimatorPose().getX() >= 8.1) {
+      reachedCenter = true;
+    }
+
+    if (reachedCenter
+        && drive.getPoseEstimatorPose().getX() <= 8.05
+        && !endEffectorTrigger.getAsBoolean()) {
+      return setStopped();
+    }
+
+    if (chassisSpeeds != null) {
+      // TODO: Check for note in intake.
+      drive.setVelocityOverride(chassisSpeeds);
+      return null;
+    }
+
+    if (scoreSpeaker.isFinished()) {
+      return setDone();
+    }
+
+    return null;
+  }
+}
