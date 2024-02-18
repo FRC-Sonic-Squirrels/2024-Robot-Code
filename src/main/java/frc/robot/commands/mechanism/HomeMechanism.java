629c4b414efb7319b2add4beb3d5b0879cfbc383
Tim <timellis337@gmail.com>
2024-02-18 15:40:12 -0800

    go full voltage control for homing, no safety positioning

--- src/main/java/frc/robot/commands/mechanism/HomeMechanism.java
+++ src/main/java/frc/robot/commands/mechanism/HomeMechanism.java
@@ -4,9 +4,7 @@
 
 package frc.robot.commands.mechanism;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.elevator.Elevator;
 
@@ -19,9 +17,6 @@ public class HomeMechanism extends Command {
   private boolean elevatorReset = false;
   private boolean beginHomingArm = false;
   private final double safeElevatorHeightForArmResetting = 5.0;
-  private final Rotation2d safeArmAngle =
-      Constants.ArmConstants.HOME_POSITION.plus(Rotation2d.fromDegrees(2.0));
-  private final double safeElevatorHeight = 1.0;
   private final double homingVoltage = -0.1;
   private final double homingVelocityMaxToResetElevator = 0.02;
   private final double homingVelocityMaxToResetArm = 0.02;
@@ -44,34 +39,20 @@ public class HomeMechanism extends Command {
   public void execute() {
     if (armReset) {
       if (!elevatorReset) {
-        if (homeElevator) {
-          elevator.setVoltage(homingVoltage);
-          if (Math.abs(elevator.getVoltage()) >= Math.abs(homingVoltage) / 2.0
-              && Math.abs(elevator.getVelocity()) <= homingVelocityMaxToResetElevator) {
-            elevator.resetSensorToHomePosition();
-            elevatorReset = true;
-          }
-        } else {
-          elevator.setHeight(safeElevatorHeight);
-          if (elevator.isAtTarget(safeElevatorHeight)) {
-            homeElevator = true;
-          }
+        elevator.setVoltage(homingVoltage);
+        if (Math.abs(elevator.getVoltage()) >= Math.abs(homingVoltage) / 2.0
+            && Math.abs(elevator.getVelocity()) <= homingVelocityMaxToResetElevator) {
+          elevator.resetSensorToHomePosition();
+          elevatorReset = true;
         }
       }
     } else {
       if (beginHomingArm) {
-        if (homeArm) {
-          arm.setVoltage(homingVoltage);
-          if (Math.abs(arm.getVoltage()) >= Math.abs(homingVoltage) / 2.0
-              && Math.abs(arm.getVelocity()) <= homingVelocityMaxToResetArm) {
-            arm.resetSensorToHomePosition();
-            armReset = true;
-          }
-        } else {
-          if (arm.isAtTargetAngle(safeArmAngle)) {
-            homeArm = true;
-          }
-          arm.setAngle(safeArmAngle);
+        arm.setVoltage(homingVoltage);
+        if (Math.abs(arm.getVoltage()) >= Math.abs(homingVoltage) / 2.0
+            && Math.abs(arm.getVelocity()) <= homingVelocityMaxToResetArm) {
+          arm.resetSensorToHomePosition();
+          armReset = true;
         }
       } else {
         elevator.setHeight(safeElevatorHeightForArmResetting);
