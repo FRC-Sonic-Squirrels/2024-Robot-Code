5e6ffba5489f668ab56a6291980dbae6bdd9ba8d
Tim <timellis337@gmail.com>
2024-02-19 09:32:16 -0800

    add requirements and interrupt behaviour to mechanism actions

--- src/main/java/frc/robot/commands/mechanism/MechanismActions.java
+++ src/main/java/frc/robot/commands/mechanism/MechanismActions.java
@@ -4,6 +4,8 @@ import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants;
 import frc.robot.commands.mechanism.MechanismPositions.MechanismPosition;
 import frc.robot.subsystems.arm.Arm;
@@ -30,29 +32,29 @@ public class MechanismActions {
 
   private static Command goToPositionParallel(
       Elevator elevator, Arm arm, Supplier<MechanismPosition> position) {
-    return new Command() {
-      MechanismPosition targetPosition;
 
-      @Override
-      public void execute() {
-        targetPosition = position.get();
-        Measure<Distance> safeHeight = Constants.ElevatorConstants.SAFE_HEIGHT;
+    return Commands.run(
+            () -> {
+              MechanismPosition targetPosition = position.get();
+              Measure<Distance> safeHeight = Constants.ElevatorConstants.SAFE_HEIGHT;
 
-        if (elevator.getHeightInches() >= safeHeight.in(Units.Inches))
-          arm.setAngle(targetPosition.armAngle());
+              if (elevator.getHeightInches() >= safeHeight.in(Units.Inches))
+                arm.setAngle(targetPosition.armAngle());
 
-        if (targetPosition.elevatorHeight().lte(safeHeight)
-            && arm.getAngle().getRadians() >= Constants.ArmConstants.AMP_SAFE_ANGLE.getRadians()) {
-          elevator.setHeight(safeHeight);
-        } else {
-          elevator.setHeight(targetPosition.elevatorHeight());
-        }
-      }
-
-      @Override
-      public boolean isFinished() {
-        return elevator.isAtTarget(targetPosition.elevatorHeight()) && arm.isAtTargetAngle();
-      }
-    };
+              if (targetPosition.elevatorHeight().lte(safeHeight)
+                  && arm.getAngle().getRadians()
+                      >= Constants.ArmConstants.AMP_SAFE_ANGLE.getRadians()) {
+                elevator.setHeight(safeHeight);
+              } else {
+                elevator.setHeight(targetPosition.elevatorHeight());
+              }
+            },
+            elevator,
+            arm)
+        .until(
+            () ->
+                elevator.isAtTarget(position.get().elevatorHeight())
+                    && arm.isAtTargetAngle(position.get().armAngle()))
+        .withInterruptBehavior(InterruptionBehavior.kCancelSelf);
   }
 }
