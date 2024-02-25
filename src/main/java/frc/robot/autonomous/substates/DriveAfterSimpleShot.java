149409894a45834647f769067cdd9acfaed88a5c
Sonic Squirrels Programming <sonicsquirrels@gmail.com>
2024-02-25 11:49:32 -0800

    drive distance instead of time

--- src/main/java/frc/robot/autonomous/substates/DriveAfterSimpleShot.java
+++ src/main/java/frc/robot/autonomous/substates/DriveAfterSimpleShot.java
@@ -1,15 +1,22 @@
 package frc.robot.autonomous.substates;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.Timer;
+import frc.lib.team2930.AllianceFlipUtil;
 import frc.lib.team2930.StateMachine;
+import frc.lib.team6328.GeomUtil;
 import frc.lib.team6328.LoggedTunableNumber;
+import frc.robot.Constants;
+import frc.robot.commands.drive.DriveToPose;
 import frc.robot.subsystems.swerve.DrivetrainWrapper;
 
 public class DriveAfterSimpleShot extends StateMachine {
   DrivetrainWrapper drive;
+  DriveToPose driveToPose;
   Timer timer = new Timer();
-  private LoggedTunableNumber driveTimer = new LoggedTunableNumber("Autonomous/driveTime", 1.5);
+  private LoggedTunableNumber driveDistance = new LoggedTunableNumber("Autonomous/SimpleShot/DistanceMeters", 1.5);
 
   public DriveAfterSimpleShot(DrivetrainWrapper drive) {
     this.drive = drive;
@@ -18,16 +25,18 @@ public class DriveAfterSimpleShot extends StateMachine {
 
   private StateHandler startTimer() {
     timer.start();
-    return this::drive;
+    driveToPose = new DriveToPose(drive, () -> 
+      {
+      double distance = Constants.isRedAlliance() ? -driveDistance.get() : driveDistance.get();
+      return drive.getPoseEstimatorPose().plus(GeomUtil.poseToTransform(new Pose2d(distance, 0.0, new Rotation2d())));
+      }
+    );
+    spawnCommand(driveToPose, (command) -> {return setDone();});
+    return this::driving;
   }
 
-  private StateHandler drive() {
-    drive.setVelocityOverride(
-        ChassisSpeeds.fromFieldRelativeSpeeds(1.0, 0.0, 0.0, drive.getRotation()));
-    if (timer.get() >= driveTimer.get()) {
-      drive.resetVelocityOverride();
-      return setDone();
-    }
+  private StateHandler driving() {
+
     return null;
   }
 }
