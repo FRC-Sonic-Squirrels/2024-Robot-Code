77b0e81c6661acc4a4e96f3fcc3cdabac930b9c6
Davide Massarenti <davide.m@corestack.io>
2024-03-10 20:00:51 -0700

    ExecutionTiming: switched to real-time timestamps

--- src/main/java/frc/lib/team2930/ExecutionTiming.java
+++ src/main/java/frc/lib/team2930/ExecutionTiming.java
@@ -1,22 +1,21 @@
 package frc.lib.team2930;
 
-import edu.wpi.first.wpilibj.Timer;
 import org.littletonrobotics.junction.Logger;
 
 public class ExecutionTiming implements AutoCloseable {
   private static final String ROOT_TABLE = "ExecutionTiming/";
 
   private final String context;
-  private final double startTime;
+  private final long startTime;
 
   public ExecutionTiming(String context) {
     this.context = context;
-    startTime = Timer.getFPGATimestamp();
+    startTime = Logger.getRealTimestamp();
   }
 
   @Override
   public void close() {
-    var endTime = Timer.getFPGATimestamp();
+    var endTime = Logger.getRealTimestamp();
 
     Logger.recordOutput(ROOT_TABLE + context, endTime - startTime);
   }
