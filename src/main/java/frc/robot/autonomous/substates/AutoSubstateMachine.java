5e071bbee3e84d2305f2eb2a9083f562de6af9b0
LuminousLlama <keyushattarde@gmail.com>
2024-04-01 01:50:58 -0700

    log conditions for vision drive to gamepiece

--- src/main/java/frc/robot/autonomous/substates/AutoSubstateMachine.java
+++ src/main/java/frc/robot/autonomous/substates/AutoSubstateMachine.java
@@ -9,6 +9,8 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.lib.team2930.LoggerEntry;
+import frc.lib.team2930.LoggerGroup;
 import frc.lib.team2930.StateMachine;
 import frc.lib.team2930.TunableNumberGroup;
 import frc.lib.team6328.LoggedTunableNumber;
@@ -49,19 +51,27 @@ public abstract class AutoSubstateMachine extends StateMachine {
   protected Translation2d gamepieceTranslation;
   private Translation2d lastSeenGamepiece;
 
+  private static final LoggerGroup logGroupGPVision = LoggerGroup.build("AutoGamepieceVision");
+  private static final LoggerEntry.Text log_GPVisionStatus = logGroupGPVision.buildString("status");
+  private static final LoggerEntry.Decimal log_GPVisionDetectedNoteDistanceFromExpectedTarget =
+      logGroupGPVision.buildDecimal("detectedNoteDistanceFromExpectedTarget");
+  private static final LoggerEntry.Decimal log_GPVisionDistanceFromRobot =
+      logGroupGPVision.buildDecimal("distanceFromRobot");
+  private static final LoggerEntry.Bool log_GPVisionWithinDistanceToRobot =
+      logGroupGPVision.buildBoolean("withinDistanceToRobot");
+  private static final LoggerEntry.Bool log_GPVisionDetectedNoteWithinExpectedRange =
+      logGroupGPVision.buildBoolean("detectedNoteWithinExpectedRange");
+
   private static final TunableNumberGroup groupTunable =
       new TunableNumberGroup("AutoSubstateMachine");
-
   private static final LoggedTunableNumber distToBeginDriveToGamepiece =
       groupTunable.build("distToBeginDriveToGamepiece", 2.0);
   private static final LoggedTunableNumber confirmationTime =
       groupTunable.build("confirmationTime", 0.8);
   protected static final LoggedTunableNumber slowDownFactor =
       groupTunable.build("slowDownFactor", 1.0);
-
   private static final LoggedTunableNumber distFromExpectedToAcceptVisionGamepiece =
       groupTunable.build("distFromExpectedToAcceptVisionGamepiece", 1.0);
-
   protected static final LoggedTunableNumber minVelToPause =
       groupTunable.build("minVelToPause", 2.0);
 
@@ -174,18 +184,36 @@ public abstract class AutoSubstateMachine extends StateMachine {
 
   protected boolean useVisionForGamepiece() {
     if (!useVision) {
+      log_GPVisionStatus.info("disabled");
+      log_GPVisionDetectedNoteDistanceFromExpectedTarget.info(-1);
+      log_GPVisionDistanceFromRobot.info(-1);
       return false;
     }
 
     ProcessedGamepieceData gamepieceData = closestGamepiece.get();
     if (gamepieceData == null) {
+      log_GPVisionStatus.info("data null");
+      log_GPVisionDetectedNoteDistanceFromExpectedTarget.info(-1);
+      log_GPVisionDistanceFromRobot.info(-1);
       return false;
     }
 
     Pose2d robotPose = drive.getPoseEstimatorPose(true);
     double distanceToRobot = gamepieceData.getDistance(robotPose).in(Units.Meters);
     double distanceToTarget = gamepieceData.getDistance(gamepieceTranslation).in(Units.Meters);
-    return distanceToRobot <= distToBeginDriveToGamepiece.get()
-        && distanceToTarget <= distFromExpectedToAcceptVisionGamepiece.get();
+
+    var withInBeginDriveDistance = distanceToRobot <= distToBeginDriveToGamepiece.get();
+    var withInExpectedTargetDistance =
+        distanceToTarget <= distFromExpectedToAcceptVisionGamepiece.get();
+
+    log_GPVisionStatus.info(
+        withInBeginDriveDistance && withInExpectedTargetDistance ? "SUCCESSFUL" : "FAILED_CHECKS");
+
+    log_GPVisionDetectedNoteDistanceFromExpectedTarget.info(distanceToTarget);
+    log_GPVisionDistanceFromRobot.info(distanceToRobot);
+    log_GPVisionWithinDistanceToRobot.info(withInBeginDriveDistance);
+    log_GPVisionDetectedNoteWithinExpectedRange.info(withInExpectedTargetDistance);
+
+    return withInBeginDriveDistance && withInExpectedTargetDistance;
   }
 }
