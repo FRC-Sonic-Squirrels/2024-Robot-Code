e2fd2d54d24a709a69bc0494164575cb14e13d78
Sonic Squirrels Programming <sonicsquirrels@gmail.com>
2024-04-04 08:26:06 -0700

    temporarily remove swerve brake mode call

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -777,7 +777,7 @@ public class RobotContainer {
                       elevator.setNeutralMode(NeutralModeValue.Coast);
                       shooter.setNeutralMode(NeutralModeValue.Coast);
                       elevator.setReactionArmIdleMode(IdleMode.kCoast);
-                      drivetrain.setNeturalMode(NeutralModeValue.Coast);
+                    //   drivetrain.setNeturalMode(NeutralModeValue.Coast);
                       brakeModeTriggered = false;
                     },
                     elevator,
@@ -790,7 +790,7 @@ public class RobotContainer {
                       elevator.setNeutralMode(NeutralModeValue.Brake);
                       shooter.setNeutralMode(NeutralModeValue.Brake);
                       elevator.setReactionArmIdleMode(IdleMode.kBrake);
-                      drivetrain.setNeturalMode(NeutralModeValue.Brake);
+                    //   drivetrain.setNeturalMode(NeutralModeValue.Brake);
                       brakeModeTriggered = true;
                     },
                     elevator,
